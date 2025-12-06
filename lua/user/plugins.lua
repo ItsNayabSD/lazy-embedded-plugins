f39227e0110e88aee5956f21a38d738cbcd82638
Nayab Sayed <basha@nayab.dev>
2025-12-07 01:39:18 +0530

    yetone/avante.nvim: ollama config added
    Signed-off-by: Nayab Sayed <basha@nayab.dev>

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -19,25 +19,15 @@ return {
             -- this file can contain specific instructions for your project
             instructions_file = "avante.md",
             -- for example
-            provider = "claude",
+            provider = "ollama",
             providers = {
-                claude = {
-                    endpoint = "https://api.anthropic.com",
-                    model = "claude-sonnet-4-20250514",
-                    timeout = 30000, -- Timeout in milliseconds
-                    extra_request_body = {
-                        temperature = 0.75,
-                        max_tokens = 20480,
-                    },
-                },
-                moonshot = {
-                    endpoint = "https://api.moonshot.ai/v1",
-                    model = "kimi-k2-0711-preview",
-                    timeout = 30000, -- Timeout in milliseconds
-                    extra_request_body = {
-                        temperature = 0.75,
-                        max_tokens = 32768,
-                    },
+                ollama = {
+                    -- Make sure endpoint is running and model downloaded
+                    endpoint = "http://127.0.0.1:11434",
+                    model = "deepseek-r1:8b",
+                    is_env_set = function()
+                        return true
+                    end,
                 },
             },
         },
